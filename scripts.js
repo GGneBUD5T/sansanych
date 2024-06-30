b37b2b67f23b0ad8f3209faee2796a6314116249
GGneBUD5T <68132167+GGneBUD5T@users.noreply.github.com>
2024-06-30 05:54:34 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-// Управление аудиофайлом
-var audio = document.getElementById('background-music');
-audio.volume = 0.3; // Устанавливаем громкость на 30%
-
-// Перезагрузка аудиофайла, чтобы он продолжал играть после завершения
-audio.addEventListener('ended', function() {
-    this.currentTime = 0;
-    this.play();
-}, false);
